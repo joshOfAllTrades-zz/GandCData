1bfd0c0c310c87467f48c9cb13c99cd779b7821e
Joshua Tharp <joshua_tharp@yahoo.com>
2014-10-20 19:33:42 -0700

    Steps 1 - 4 complete.

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,109 @@
-# 1 Merge the training and the test sets to create one data set
+####
+# Constants and paths for use later
+srcHost <- "https://d396qusza40orc.cloudfront.net"
+srcPath <- "/getdata%2Fprojectfiles%2F"
+srcFile <- "UCI%20HAR%20Dataset.zip"
+srcUrl <- paste(srcHost, srcPath, srcFile, sep = "")
+
+dataDir <- "data"
+file <- "uciDataset.zip"
+destFile <- paste(dataDir, file, sep = "/")
+
+unzipDir <- "UCI HAR Dataset"
+
+# Features
+featuresFile <- paste(dataDir, unzipDir, "features.txt", sep = "/")
+
+# Activity Catalog
+activitiesFile <- paste(dataDir, unzipDir, "activity_labels.txt", sep = "/")
+
+# Training files
+trainDir     <- paste(dataDir, unzipDir, "train", sep = "/")
+trainSbjFile <- paste(trainDir, "subject_train.txt", sep = "/")
+trainXFile   <- paste(trainDir, "X_train.txt", sep = "/")
+trainYFile   <- paste(trainDir, "Y_train.txt", sep = "/")
+
+# Testing files
+testDir     <- paste(dataDir, unzipDir, "test", sep = "/")
+testSbjFile <- paste(testDir, "subject_test.txt", sep = "/")
+testXFile   <- paste(testDir, "X_test.txt", sep = "/")
+testYFile   <- paste(testDir, "Y_test.txt", sep = "/")
+
+####
+# Get the raw data
+
+# Make sure the data directory exists; create it if it doesn't. Further, assume
+# that we need to download and unpack the data. Conversely, if the directory
+# exists, assume that we already have gotten the data.
+if (!file.exists(dataDir)) {
+  dir.create(dataDir)
+  download.file(srcUrl, destfile = destFile)
+  unzip(destFile, exdir = dataDir)
+}
+
+####
+# Read in the relevant data files for further manipulation
+
+# Start with the subject files as these are simple, 1-field wide tables
+trainSbj <- read.csv(trainSbjFile, header = FALSE)
+testSbj  <- read.csv(testSbjFile, header = FALSE)
+
+# Get the names of the features (variables), and find the ones that contain
+# either the word "mean" or "Mean". I'm going to use these columns to determine
+# which features to read from the training and testing data.
+features <- read.table(featuresFile, sep = " ")
+means <- grepl("mean()", features$V2)
+stds  <- grepl("std()", features$V2)
+keeperFields <- means | stds
+fieldNames <- features$V2[keeperFields]
+
+# When reading fixed width fields a negative number means to skip that many
+# characters. In our situation all fields are 16 characters wide so I'm
+# creating an array of 16 and -16 based on the fields of interest.
+fields <- ifelse(keeperFields, 16, -16)
+
+# Read in the training and testing data using our reduced fields. Again,
+# instead of reading in the full 561 fields, I'm only reading in those fields
+# that have the word "mean" or "Mean" in its feature name.
+# ******** Satisfies Step 2 by only reading the mean() and std() fields ********
 # 2 Extract only the measurements on the mean and standard deviation for each
 #   measurement
+trainX   <- read.fwf(trainXFile, fields, header = FALSE)
+trainY   <- read.csv(trainYFile, header = FALSE)
+testX    <- read.fwf(testXFile, fields, header = FALSE)
+testY    <- read.csv(testYFile, header = FALSE)
+
+# Read in the activity catalog
+actCat <- read.table(activitiesFile,
+                     header = FALSE,
+                     sep = " ",
+                     colClasses = c("numeric", "character"))
+
+####
+# Combine the data into a single, large data frame.
+
+# Time to bind the subjects, activity, and test/train data
+# ******** Satisifies Step 1 by merging the training and test sets ********
+# 1 Merge the training and the test sets to create one data set
+data <- rbind(cbind(testSbj, testY, testX),
+              cbind(trainSbj, trainY, trainX))
+
+####
+# Do a little labeling of the data
+
+# Convert the activities to factors and provide the levels
+# ******** Satisfies Step 3 by providing activity names in the data ********
 # 3 Use descriptive activity names to name the activities in the data set
+data$Activity <- factor(data$Activity)
+levels(data$Activity) <- actCat$V2
+
+# Assign features as the column names
+# ******** Satisfies Step 4 by labeling the variables ********
 # 4 Appropriately label the data set with descriptive variable names
+names(data) <- union(c("Subject", "Activity"), fieldNames)
+
+####
+# Create my new tidy data set
+
 # 5 From the data set in step 4, create a second, independent tidy data set
 #   with the average of each variable for each activity and each subject
